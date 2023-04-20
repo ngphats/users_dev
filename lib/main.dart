cc33ba566b46f887ca6a1d557b41bf9f03b2149b
phatnn <ngphats@gmail.com>
2023-04-20 22:21:10 +0700

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
 // import './user_edit_form.dart';
 
-// void main() => runApp(const MyApp());
-void main() => runApp(MaterialApp(
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: const Text('I Am Rich'),
           backgroundColor: Colors.blueGrey[900],
+          title: const Text("I'm Rich"),
         ),
-        body: const Center(
-          child: Image(
-            image: AssetImage('images/demo.jpg'),
-          ),
-        ),
+        body: const Processing(),
       ),
-    ));
+    );
+  }
+}
+
+class Processing extends StatefulWidget {
+  const Processing({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return BodyHome();
+  }
+}
+
+class BodyHome extends State {
+  int _count = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('You have pressed the button $_count times.'),
+    );
+  }
+}
+
+void main() => runApp(const MyApp());
